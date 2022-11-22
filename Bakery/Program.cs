7eea3fdff68409c7553a00b65f76b7f397225c83
gramsower <110431307+gramsower@users.noreply.github.com>
2022-11-21 18:04:10 -0800

    add UI logic describing available items and pricing

---
+++ Bakery/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+// using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("How many pastries or loaves of bread would you like? \nBread: $5 per loaf \nPastry: $2 each. \n****************** \nBuy two loaves and get the third one FREE (applies to every third loaf purchased.) \n****************** \nBuy three to five pastries and recieve a dollar off your order. \nBuy six or more pastries and get one free!");
+    }
+  }
+}
+
