1eb43b93e28d3a552382fa2a1e55b0edc3e98484
gramsower <110431307+gramsower@users.noreply.github.com>
2022-11-23 18:11:50 -0800

    add additional functionality to UI and update README

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,27 +7,44 @@ namespace Bakery
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("\nWelcome to Pierre's Bakery!");
       Console.WriteLine("\n****************** \nWe Offer Bread And Pastries. \n~Bread: $5 per loaf \n~Pastry: $2 each \n****************** \n\nSPECIALS! \n\n~Buy two loaves and get the third one FREE (applies to every third bread loaf purchased). \n\n~Buy three, four, or five pastries and recieve a dollar off your order total. \n~Buy six or more pastries and get one free! \n******************");
 
-      Console.WriteLine("How many bread loaves would you like today?");
+      Console.WriteLine("How many bread loaves would you like today? (If none, enter 0).");
       string breadInput = Console.ReadLine();
       int numberLoaves = int.Parse(breadInput);
-      Console.WriteLine($"OK, {numberLoaves} loaves, coming right up!");
+      if (numberLoaves >1)
+      {
+        Console.WriteLine($"OK, {numberLoaves} loaves, coming right up!");
+      }
+      else
+      {
+        Console.WriteLine($"OK, just {numberLoaves} loaf, coming right up!");;
+      }
       Bread breadOrder = new Bread(numberLoaves);
       int breadCost = breadOrder.BreadCost();
-
-      Console.WriteLine("And how many pastries would you like today?");
+      
+      Console.WriteLine("And how many pastries would you like today? (If none, enter 0.");
       string pastryInput = Console.ReadLine();
       int numberPastries = int.Parse(pastryInput);
-      Console.WriteLine($"OK, {numberPastries} pastries on the way!");
+      if (numberPastries >1)
+      {
+        Console.WriteLine($"OK, {numberPastries} pastries on the way!");
+      }
+      else
+      {
+        Console.WriteLine($"OK...I guess... only {numberPastries} pastry on the way! You certainly have a lot of self control!");
+      }
       Pastry pastryOrder = new Pastry(numberPastries);
       int pastryCost = pastryOrder.PastryCost();
 
       int totalCost = pastryCost + breadCost;
+      int rawTotal = (numberLoaves*5) + (numberPastries*2);
+      int totalSavings = rawTotal - totalCost;
+
+      Console.WriteLine($"Your bread subtotal is: ${breadCost}.00");
       Console.WriteLine($"Your pastry subtotal is: ${pastryCost}.00");
-      Console.WriteLine($"By the way, your bread subtotal is: ${breadCost}.00");
-      Console.WriteLine($"Your total is ${totalCost}.00. \nThank you for your support!");
+      Console.WriteLine($"Your total is ${totalCost}.00. \nYou saved {totalSavings}.00 today! \nThank you for your support of local businesses like ours!");
 
       Console.WriteLine("Would you like to place another order? (enter y/n)");
       string userRerun = Console.ReadLine();
--- README.md
+++ README.md
@@ -15,7 +15,8 @@
 * This application runs in the console.  Upon startup, the user is greeted with a message welcoming them to Pierre's Bakery and displaying the menu.
 * The menu consists of two items: bread and pastry. Depending upon the customer's order size, the customer may recieve a discount on either or both types of item.
 * The user is not required to order both types of items, but must enter '0' for any item type they do not want.
-* The application then returns the subtotal for each type of item, and the overall combined total.
+* The program will return a confirmation of the number of each item, using correct grammar (i.e., "pastry" when a single pastry is ordered, "pastries" in all other circumstances.)
+* The application then returns the subtotal for each type of item, the overall total, and the amount the user saved due to the specials.
 * The user is then prompted to enter another order, or quit the application.
 
 ## Setup/Installation Requirements
@@ -28,11 +29,12 @@
 * Open the application using the command 'dotnet run'.
 * Follow the application prompts.
 
-## Known Bugs & Limitations
+## Known Bugs, Limitations, and Easter Eggs
 
 * Very limited error checking.
-* Specifically, the program will crash and terminate if the user enters any other character than a whole positive integer.
+* Specifically, the program will crash and terminate if the user enters any other character than a whole positive integer when asked how many of each item the user would like to order.
 * UI is rudimentary.
+* If the user only orders a single pastry, the program will display a special message.
 
 ## License
 [GNU GPL 3.0](https://choosealicense.com/licenses/gpl-3.0/) Copyright (c) 11/14/2022 Greg Ramsower
